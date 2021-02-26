4d01b6cb7ab7bb458f635b28d8f73982f5b65200
SigureMo <sigure.qaq@gmail.com>
2021-02-26 18:04:12 +0800

    ⚡️ perf: use zimfw instead of ohmyzsh & using new theme pure

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,17 @@
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/SigureMo/dotfiles/master/bootstrap.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/SigureMo/dotfiles/main/bootstrap.sh)"
 
 # clone this repo
 git clone https://github.com/SigureMo/dotfiles ~/.dotfiles
 . ~/.dotfiles/zsh/functions.zsh
 
 # init oh my zsh and plugins
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 install_autojump
 
 # Source dot files
 echo '. ~/.dotfiles/zsh/.zprofile' >> ~/.zprofile
 source ~/.zprofile
-echo '. ~/.dotfiles/zsh/.zshrc' > ~/.zshrc
+echo '. ~/.dotfiles/zsh/.zshrc' >> ~/.zshrc
 source ~/.zshrc
+echo '. ~/.dotfiles/zsh/.zimrc' > ~/.zimrc
+zimfw install
