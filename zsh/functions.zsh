99212ccddac151d576488d0b1db4704f5287ce72
SigureMo <sigure.qaq@gmail.com>
2022-02-20 22:30:10 +0800

    ✨ feat: add nya shortcuts

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -30,3 +30,80 @@ function install_autojump() {
             ;;
     esac
 }
+
+function nya() {
+    action=$1
+    app=$2
+    case $action in
+        "clean")
+        echo "> clean $app"
+        case $app in
+            "brew")
+            brew autoremove
+            ;;
+            "pnpm")
+            pnpm store prune
+            ;;
+            "cargo")
+            # need `cargo install cargo-cache`
+            cargo cache -a
+            ;;
+            "conda")
+            conda clean --all -y
+            ;;
+            "-A")
+            nya clean brew
+            nya clean pnpm
+            nya clean cargo
+            nya clean conda
+            ;;
+            *)
+            echo "Unknown app: $app"
+            ;;
+        esac
+        ;;
+        "upgrade")
+        echo "> upgrade $app"
+        case $app in
+            "brew")
+            brew autoremove
+            ;;
+            "pnpm")
+            pnpm store prune
+            ;;
+            "cargo")
+            # need `cargo install cargo-update`
+            cargo install-update -a
+            ;;
+            "rustup")
+            rustup upgrade
+            ;;
+            "zimfw")
+            zimfw update
+            zimfw upgrade
+            ;;
+            "conda")
+            conda update --all -y
+            ;;
+            "-A")
+            nya upgrade brew
+            nya upgrade pnpm
+            nya upgrade cargo
+            nya upgrade rustup
+            nya upgrade zimfw
+            ;;
+            *)
+            echo "Unknown app: $app"
+            ;;
+        esac
+        ;;
+        "-A")
+        nya upgrade -A
+        nya clean -A
+        ;;
+        *)
+        echo "Unknown action: $action"
+        ;;
+    esac
+}
+
