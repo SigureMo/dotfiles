92c9daad40eb631bc4bc82d3676f5801cd93143b
SigureMo <sigure.qaq@gmail.com>
2022-02-25 19:45:15 +0800

    💄 feat: add colored echo

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,34 +1,69 @@
 #!/usr/bin/zsh
 
-if uname -a | grep -qwi "darwin"; then
-    os_name="macos"
-elif uname -a | grep -qwi "manjaro"; then
-    os_name="manjaro"
-elif uname -a | grep -qwi "azure"; then
-    os_name="codespace"
-else
-    os_name="unknown"
-fi
-
 function donothing() {
     :
 }
 
-function install_autojump() {
-    case $os_name in
-        "macos")
-            brew install autojump
-            ;;
-        "manjaro")
-            sudo pacman -S autojump
-            ;;
-        "codespace")
-            sudo apt install autojump
-            ;;
-        "unknown")
-            donothing
-            ;;
+function colored-echo() {
+    CSI="\x1b["
+    code=""
+    msg=$1
+    fore=$2
+    back=$3
+    # styles=${@:4}
+    style=$4
+    case $fore in
+        "black") code="${code};30";;
+        "red") code="${code};31";;
+        "green") code="${code};32";;
+        "yellow") code="${code};33";;
+        "blue") code="${code};34";;
+        "magenta") code="${code};35";;
+        "cyan") code="${code};36";;
+        "white") code="${code};37";;
+        "bright_black") code="${code};90";;
+        "bright_red") code="${code};91";;
+        "bright_green") code="${code};92";;
+        "bright_yellow") code="${code};93";;
+        "bright_blue") code="${code};94";;
+        "bright_magenta") code="${code};95";;
+        "bright_cyan") code="${code};96";;
+        "bright_white") code="${code};97";;
+        "defaultfg") code="${code};39";;
     esac
+
+    case $back in
+        "black") code="${code};40";;
+        "red") code="${code};41";;
+        "green") code="${code};42";;
+        "yellow") code="${code};43";;
+        "blue") code="${code};44";;
+        "magenta") code="${code};45";;
+        "cyan") code="${code};46";;
+        "white") code="${code};47";;
+        "bright_black") code="${code};100";;
+        "bright_red") code="${code};101";;
+        "bright_green") code="${code};102";;
+        "bright_yellow") code="${code};103";;
+        "bright_blue") code="${code};104";;
+        "bright_magenta") code="${code};105";;
+        "bright_cyan") code="${code};106";;
+        "bright_white") code="${code};107";;
+        "defaultbg") code="${code};49";;
+    esac
+
+    case $style in
+        "bold") code="${code};1";;
+        "italic") code="${code};3";;
+        "underline") code="${code};4";;
+    esac
+
+    # for style in ${styles[@]}
+    # do
+    #     echo "${style}"
+    # done
+
+    echo "${CSI}${code}m${msg}${CSI}0m"
 }
 
 function nya() {
@@ -36,10 +71,11 @@ function nya() {
     app=$2
     case $action in
         "clean")
-        echo "> clean $app"
+        colored-echo "> clean $app" blue defaultbg bold
         case $app in
             "brew")
             brew autoremove
+            brew cleanup --prune=7 -s
             ;;
             "pnpm")
             pnpm store prune
@@ -58,12 +94,12 @@ function nya() {
             nya clean conda
             ;;
             *)
-            echo "Unknown app: $app"
+            colored-echo "Unknown app: $app" red
             ;;
         esac
         ;;
         "upgrade")
-        echo "> upgrade $app"
+        colored-echo "> upgrade $app" magenta defaultbg bold
         case $app in
             "brew")
             brew autoremove
@@ -93,7 +129,7 @@ function nya() {
             nya upgrade zimfw
             ;;
             *)
-            echo "Unknown app: $app"
+            colored-echo "Unknown app: $app" red
             ;;
         esac
         ;;
@@ -102,7 +138,7 @@ function nya() {
         nya clean -A
         ;;
         *)
-        echo "Unknown action: $action"
+        colored-echo "Unknown action: $action" red
         ;;
     esac
 }
